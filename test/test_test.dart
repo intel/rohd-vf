40f2f270d0e34d18dc8132ad9fed3deb1a2cb844
Max Korbel <max.korbel@intel.com>
2022-11-28 08:19:59 -0800

    Improvements for `Test` failures via `Logger` (#28)

--- test/test_test.dart
+++ test/test_test.dart
@@ -9,14 +9,13 @@
 ///
 
 import 'dart:async';
-
 import 'package:logging/logging.dart';
 import 'package:rohd/rohd.dart';
 import 'package:rohd_vf/rohd_vf.dart';
 import 'package:test/test.dart';
 
 class ForeverObjectionTest extends Test {
-  ForeverObjectionTest() : super('foreverObjectionTest');
+  ForeverObjectionTest() : super('foreverObjectionTest', printLevel: Level.OFF);
 
   @override
   Future<void> run(Phase phase) async {
@@ -38,9 +37,54 @@ class NormalTest extends Test {
   }
 }
 
+class LogTypeTest extends Test {
+  final Level errorLevel;
+
+  bool testCompleted = false;
+
+  final Logic _clk = SimpleClockGenerator(10).clk;
+
+  LogTypeTest(this.errorLevel) : super('logTypeTest', printLevel: Level.OFF) {
+    Simulator.setMaxSimTime(500);
+  }
+
+  @override
+  Future<void> run(Phase phase) async {
+    unawaited(super.run(phase));
+
+    final obj = phase.raiseObjection();
+
+    await _clk.nextPosedge;
+
+    logger.log(errorLevel, 'error message');
+
+    await _clk.nextPosedge;
+
+    testCompleted = true;
+
+    obj.drop();
+  }
+}
+
+class CheckPhaseFailureTest extends Test {
+  CheckPhaseFailureTest()
+      : super('checkPhaseFailureTest', printLevel: Level.OFF) {
+    FailingSubComponent(this);
+  }
+}
+
+class FailingSubComponent extends Component {
+  FailingSubComponent(Component parent) : super('failingSubComponent', parent);
+
+  @override
+  void check() {
+    logger.severe('Failure during check');
+  }
+}
+
 void main() {
   setUp(() {
-    Logger.root.level = Level.OFF;
+    Logger.root.level = Level.WARNING;
   });
 
   tearDown(Simulator.reset);
@@ -52,5 +96,49 @@ void main() {
   test('Test.start does not complete until simulation ends', () async {
     await NormalTest().start();
     expect(Simulator.simulationHasEnded, isTrue);
+  });
+
+  test('Logger failure causes exception at end of test', () async {
+    var sawError = false;
+    final logTest = LogTypeTest(Level.SEVERE);
+
+    try {
+      await logTest.start();
+    } on Exception {
+      sawError = true;
+    }
+
+    expect(sawError, isTrue);
+    expect(logTest.testCompleted, isTrue);
+  });
+
+  test('Logger kill causes exception immediately', () async {
+    var sawError = false;
+    final logTest = LogTypeTest(Level.SHOUT);
+
+    try {
+      await logTest.start();
+    } on Exception {
+      sawError = true;
+    }
+
+    expect(sawError, isTrue);
+    expect(logTest.testCompleted, isFalse);
+  });
+
+  test('Logger normal message passes', () async {
+    await LogTypeTest(Level.WARNING).start();
+  });
+
+  test('Failure during check causes test failure', () async {
+    var sawError = false;
+
+    try {
+      await CheckPhaseFailureTest().start();
+    } on Exception {
+      sawError = true;
+    }
+
+    expect(sawError, isTrue);
   });
 }
